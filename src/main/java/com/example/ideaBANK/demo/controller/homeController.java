8ffa321ebea9e680a10e13fccb62250e88afbd98
jeonga-Son <jjeong0106@gmail.com>
2022-08-11 21:05:58 +0900

    모달창 게시판 수정필요

--- src/main/java/com/example/ideaBANK/demo/controller/homeController.java
+++ src/main/java/com/example/ideaBANK/demo/controller/homeController.java
@@ -2,7 +2,14 @@ package com.example.ideaBANK.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class homeController {
+
+    @GetMapping("/")
+    public String showHome() {
+        return "index";
+    }
 }
